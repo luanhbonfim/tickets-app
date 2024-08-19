e67cdf8137c3d7d4c031ddd89c055aab207bcb70
Luan Henrique <luanhenrique.dev@gmail.com>
2024-08-19 11:44:36 -0300

    feat: Inicializando servidor

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-import http from 'http';
-import fs from 'fs';
+import express from 'express';
 
-const hostname = 'localhost';
-const porta = 3005;
+const host = 'localhost';
+const port = 3000;
 
-const servidor = http.createServer(
-    (req, resp) => {
-        if (req.method === "GET") {
-            if (req.url === '/login.html') {
-                fs.createReadStream("./public" + req.url).pipe(resp);
-            } else {
-                resp.statusCode = 404;
-                resp.setHeader('Content-type', 'text/html;charset=utf-8');
-                resp.end(`<p>A página ${req.url}, não foi encontrada</p>`)
-            }
-        } else {
-            resp.statusCode = 405; 
-            resp.setHeader('Content-Type', 'text/html;charset=utf-8');
-            resp.end('<p>Método não permitido</p>');
-        }
+const app = express();
+
+app.use(express.static('./public'));
+
+app.listen(port, host, () => {
+    console.log("Servidor on-line.")
+    console.log(`-> http://${host}:${port}`)
 });
 
-servidor.listen(porta, hostname, () => {
-    console.log(`Servidor online! ${hostname}:${porta}`);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import http from 'http';
+// import fs from 'fs';
+
+// const hostname = 'localhost';
+// const porta = 3005;
+
+// const servidor = http.createServer(
+//     (req, resp) => {
+//         if (req.method === "GET") {
+//             if (req.url === '/login.html') {
+//                 fs.createReadStream("./public" + req.url).pipe(resp);
+//             } else {
+//                 resp.statusCode = 404;
+//                 resp.setHeader('Content-type', 'text/html;charset=utf-8');
+//                 resp.end(`<p>A página ${req.url}, não foi encontrada</p>`)
+//             }
+//         } else {
+//             resp.statusCode = 405; 
+//             resp.setHeader('Content-Type', 'text/html;charset=utf-8');
+//             resp.end('<p>Método não permitido</p>');
+//         }
+// });
+
+// servidor.listen(porta, hostname, () => {
+//     console.log(`Servidor online! ${hostname}:${porta}`);
+// });
